ecbf7a65defea3fea497fb3cb32488c20dda5cd2
jimimbong <john.imbong@webit.com.au>
2026-04-26 21:27:38 +0800

    change auth urls

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     # Override Djoser's users/me/ to support multipart avatar uploads
     path('', include('djoser.urls')),
     # Cookie-aware overrides — must come before djoser.urls.jwt
-    path('jwt/login/', views.AuthLoginView.as_view(), name='jwt-login'),
-    path('jwt/refresh/', views.AuthRegenerateTokenView.as_view(), name='jwt-refresh'),
-    path('jwt/logout/', views.AuthLogoutView.as_view(), name='jwt-logout'),
+    path('login/', views.AuthLoginView.as_view(), name='jwt-login'),
+    path('refresh/', views.AuthRegenerateTokenView.as_view(), name='jwt-refresh'),
+    path('logout/', views.AuthLogoutView.as_view(), name='jwt-logout'),
     path('', include('djoser.urls.jwt')),
 ]
