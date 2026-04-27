842195f4fa3a8a0d583e4ee7a2ebc3c0490142a4
jimimbong <john.imbong@webit.com.au>
2026-04-27 20:11:03 +0800

    set up shadcn

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -17,6 +19,20 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        {allowConstantExport: true},
+      ],
+			'@typescript-eslint/no-unused-vars': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'prefer-const': 'warn',
+			'react/no-unescaped-entities': 'off',
+			'no-extra-boolean-cast': 'off',
+			'@typescript-eslint/no-empty-object-type': 'off',
+			'@tanstack/query/exhaustive-deps': 'error',
     },
   },
 ])
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@ui": path.resolve(__dirname, "src/components/ui"),
+      "@views": path.resolve(__dirname, "src/views"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@core": path.resolve(__dirname, "src/core"),
+      "@auth": path.resolve(__dirname, "src/auth"),
+      "@config": path.resolve(__dirname, "src/core/config"),
+    },
+  },
 })
